02188d5bd0c3de2039cc07ae6ba5facac7b33302
Arturo Atencio <arturo.a.atencio@gmail.com>
2023-04-23 03:00:18 +0000

    Se crean los metodos CRUD en el .php del componente Task. || Se modifica el .blade del componente para que interactue con las funciones CRUD del .php || Se agrega un CDN para los iconos de la libreria FA.

--- app/Http/Livewire/Tasks.php
+++ app/Http/Livewire/Tasks.php
@@ -4,12 +4,81 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use App\Models\User;
+use App\Models\Task;
+
 class Tasks extends Component
 {
-    public $task = [];
+    public $tasks = [];
+
+    protected $rules = [
+        'tasks.*.title' => 'required|string|min:6',
+        'tasks.*.completed' => 'required|string|max:500',
+    ];
+
+    public function mount()
+    {
+        $this->tasks = Task::all();
+    }
 
     public function render()
     {
         return view('livewire.tasks');
+    }
+
+    public function storeTask($id)
+    {
+        
+    }
+
+    public function completeTask($id)
+    {
+        $v = $this->tasks->filter(function($item) use ($id){
+
+            return $item->id == $id;
+
+        })->first();
+
+        $v->completed = true;
+
+        $v->save();
+    }
+
+    public function editTask($id)
+    {
+        $task = Task::find($id);        
+            
+        $v = $this->tasks->filter(function($item) use ($id){
+
+            return $item->id == $id;
+
+        })->first();
+
+        if ( $task->completed ) { $v->completed = false; }
+        
+        $v->editing = true;
+    }
+
+    public function saveTask($id)
+    {
+        $task = Task::find($id);
+
+        $v = $this->tasks->filter(function($item) use ($id){
+
+            return $item->id == $id;
+
+        })->first();        
+        
+        $v->editing = false;
+        
+        $task->title = $v->title;
+        $task->completed = false;
+
+        $task->save();
+    }
+
+    public function deleteTask($id)
+    {
+        dd(36, $id);
     }
 }
--- resources/views/livewire/tasks.blade.php
+++ resources/views/livewire/tasks.blade.php
@@ -1,50 +1,65 @@
 <div>
-
-    <h2 class="text-center">Lista de Tareas</h2>
-    <br>
-
-    <form class="">
-        <div class="form-group row">
-            <div class="col-md-10">
-                <input type="text" name="addTodo" class="form-control form-control-lg" id="addTodo" placeholder="Crear Contenido del Blog"  
-                    wire:model="title" wire:keydown.enter="addTodo">
-                @if ($errors->has('title'))
-                    <div style="color:red;">{{ $errors->first('title') }}</div>
-                @endif
+    <div class="col-md-8" style="margin: auto;">        
+        <div class="card">
+            <div class="card-header">
+                <h2 class="text-center">Lista de Tareas</h2>
             </div>
-            
-            <button class="btn btn-primary col-md-2" wire:click="addTodo" type="submit">Agregar</button>
-        </div>
-    </form>
-
-
-    <ul class="list-group">
-        @foreach ($tasks as $task)
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-              <div>
-                <input type="checkbox" wire:change="toggleTodo({{$task->id}})" class="mr-4" {{$task->completed ? 'checked' : ''}}>
-                <a
-                    href="#"
-                    class="{{ $task->completed ? 'completed' : ''}}"
-                    onclick="updateTodoPrompt('{{$task->title}}') || event.stopImmediatePropagation()"
-                    wire:click="updateTodo({{$task->id}}, todoUpdated)"
-                >
-                    {{ $task->title }}
-                </a>
-              </div>
-              <div>
-
-                  <button
-                    class="btn btn-sm btn-danger"
-                    onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
-                    wire:click="deleteTodo({{ $task->id }})">
-                &times;</button>
-
-              </div>
-            </li>
-
-        @endforeach
-    </ul>
+            <div class="card-body ">
+                {{-- <h5 class="card-title">Special title treatment</h5> --}}
+                <br>
+                <form class="">
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="text" name="addTodo" class="form-control form-control-lg" id="addTodo" placeholder="Crear Contenido del Blog"  
+                                wire:model.defer="title" wire:keydown.enter="addTodo">
+                            @if ($errors->has('title'))
+                                <div style="color:red;">{{ $errors->first('title') }}</div>
+                            @endif
+                        </div>
+                        
+                        <button class="btn btn-primary col-md-2" wire:click="addTodo" type="submit">Agregar</button>
+                    </div>
+                </form>
+                <br><br>
+                <ul class="list-group">
+                    @foreach ($tasks as $k => $task)
+                        <li class="my-2 d-flex justify-content-between align-items-center"> 
+                            <div class="input-group mb-3">
+                                <div class="my-2">
+                                    <i class="fas fa-plus" aria-hidden="true"></i>
+                                </div>                                
+                                <input class="form-control mx-2" type="text" {{ $task->editing ? '' : 'disabled'}} 
+                                    wire:model.defer="tasks.{{ $k }}.title" 
+                                    style="{{ $task->completed ? 'text-decoration: line-through;' : '' }}">
+                                <div>
+                                    <button class="btn btn-md btn-outline-danger" {{ $task->editing ? 'disabled readonly' : ''}} 
+                                        onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
+                                        wire:click.prevent="deleteTask({{ $task->id }})">
+                                        <i class="fas fa-times-circle" aria-hidden="true"></i>
+                                    </button>
+                                    @if ( $task->editing ) 
+                                        <button class="btn btn-md btn-outline-info"
+                                            wire:click.prevent="saveTask({{ $task->id }})">
+                                            <i class="fas fa-save" aria-hidden="true"></i>
+                                        </button>
+                                    @else                                       
+                                        <button class="btn btn-md btn-outline-info"
+                                            wire:click.prevent="editTask({{ $task->id }})">
+                                            <i class="fas fa-pencil-alt" aria-hidden="true"></i>
+                                        </button>
+                                    @endif
+                                    <button class="btn btn-md btn-outline-success" {{ ($task->completed || $task->editing) ? 'disabled readonly' : ''}}                                    
+                                        wire:click.prevent="completeTask({{ $task->id }})">
+                                        <i class="fas fa-check" aria-hidden="true"></i>
+                                    </button>
+                                </div>                                    
+                            </div>
+                        </li>            
+                    @endforeach
+                </ul>
+            </div>
+        </div> 
+    </div>
 
 
     <script>
